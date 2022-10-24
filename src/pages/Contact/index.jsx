7921e5d10e5c616c355d3489d2509db56ffa0655
Paulo Lima <paulynho_lima@hotmail.com>
2022-10-24 15:08:06 -0300

    feat: add form em pagina de contatos

--- src/pages/Contact/index.jsx
+++ src/pages/Contact/index.jsx
@@ -1,11 +1,59 @@
-import React from 'react';
+import React, { useState } from 'react';
+import './contact.css';
+
 
 function Contact() {
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    if (name === '' || email === '' || message === '') {
+      alert('Por favor prencher os campos vazio!')
+      return;
+    };
+    alert('teste')
+  }
+
 
   return (
-    <>
-      <div>Contact</div>
-    </>
+    <main className='maincontato'>
+      <div className='containeConta'>
+        <h1 className='title'> Meus Contatos</h1>
+
+        <form className='form' onSubmit={handleSubmit}>
+          <input
+            className='input'
+            type="text"
+            placeholder='Digite seu Nome'
+            onChange={(e) => setName(e.target.value)}
+            value={name}
+          />
+
+          <input
+            className='input'
+            type="email"
+            placeholder='Digite seu Email'
+            onChange={(e) => setEmail(e.target.value)}
+            value={email}
+          />
+
+          <textarea
+            className='textearea'
+            placeholder='Deixe sua mensagem'
+            onChange={(e) => setMessage(e.target.value)}
+            value={message}
+          />
+
+          <input className='button' type="submit" value="Enviar" />
+
+        </form>
+
+      </div>
+    </main>
   );
 
 }
--- src/pages/Projects/index.jsx
+++ src/pages/Projects/index.jsx
@@ -9,6 +9,8 @@ import fotoCart from '../../assets/img/shopping-cart.png';
 import fotoWallet from '../../assets/img/trybe-wallet.png';
 import fotoCook from '../../assets/img/cookmaster.png';
 import fotoStar from '../../assets/img/star-wars.png';
+import fotoBlog from '../../assets/img/blog-posts.png';
+
 
 function Projects() {
 
@@ -32,38 +34,43 @@ function Projects() {
 
             <h1>Projeto de treinamento spring boot Java IDE STS,foi desenvovido um CRUD no back-end e ReactTs no front-end.
               Clik na imagem para interagir com interface ao entrar na pagina clik F5 para carregar o back-end :)</h1>
-            <a href='https://dsmeta-paulynho.netlify.app/' target="_blank"><img src={foto} alt="Cadastro de vendas" class="item current-item" /></a>
+            <a className='fotoVendas' href='https://dsmeta-paulynho.netlify.app/' target="_blank"><img src={foto} alt="Cadastro de vendas" class="item current-item" /></a>
 
           </SwiperSlide>
 
           <SwiperSlide>
-            <h1>Esse foi um projeto desenvolvido para colocar em pratica a manipulação do Dom javascript, html, css. Clik na imagem para interagir com as cores :)</h1>
+            <h1>Esse foi um projeto Front-End desenvolvido para colocar em pratica a manipulação do Dom javascript, html, css. Clik na imagem para interagir com as cores :)</h1>
             <a href='https://paulynho-lima.github.io/PixArt/' target="_blank"><img src={fotoPixl} alt="Pixel-Art" class="item current-item" /></a>
           </SwiperSlide>
 
           <SwiperSlide>
-            <h1>Este foi um projeto desenvolvido para colocar em pratica javascript, html, css com atributos display responsivo. Clik na imagem para add Tarefas :)</h1>
+            <h1>Este foi um projeto Front-End desenvolvido para colocar em pratica javascript, html, css com atributos display responsivo. Clik na imagem para add Tarefas :)</h1>
             <a href='https://paulynho-lima.github.io/lista-de-tarefas/' target="_blank"><img src={fotoTasks} alt="list-tasks" class="item current-item" /></a>
           </SwiperSlide>
 
           <SwiperSlide>
-            <h1>Este foi um projeto desenvolvido para colocar em pratica a manipulação Dom e consumir uma Api do MELI. Clik na imagem para adicionar Produtos :)</h1>
+            <h1>Este foi um projeto Front-End desenvolvido para colocar em pratica a manipulação Dom e consumir uma Api do MELI. Clik na imagem para adicionar Produtos :)</h1>
             <a className='fotoShopping' href=" https://paulynho-lima.github.io/shopping-cart/" target="_blank"><img src={fotoCart} alt="shopping-cart" class="item current-item" /></a>
           </SwiperSlide>
 
           <SwiperSlide>
-            <h1>Esta aplicação wallet de controle de gastos foi desenvolvido para colocar em pratica o Reactjs com Redux, Redux-Devtoos e Redux-thunk consumindo uma Api de moedas. Clik na imagem para saber mais... :)</h1>
+            <h1>Esta aplicação Front-End wallet de controle de gastos foi desenvolvido para colocar em pratica o Reactjs com Redux, Redux-Devtoos e Redux-thunk consumindo uma Api de moedas. Clik na imagem para saber mais... :)</h1>
             <a className='fotowallet' href='https://paulynho-lima.github.io/wallet/' target="_blank"><img src={fotoWallet} alt="wallet" class="item current-item" /></a>
           </SwiperSlide>
 
           <SwiperSlide>
-            <h1>Esta aplicação Star-Wars para consultar os planetas de gerra nas estrelas colocar em pratica o Reactjs com Api Context, Usestate e Useffects consumindo uma Api de Planetas. Clik na imagem para saber mais... :)</h1>
-            <a className='fotowallet' href='https://paulynho-lima.github.io/star-wars/' target="_blank"><img src={fotoStar} alt="wallet" class="item current-item" /></a>
+            <h1>Esta aplicação Front-End Star-Wars para consultar os planetas de gerra nas estrelas colocar em pratica o Reactjs com Api Context, Usestate e Useffects consumindo uma Api de Planetas. Clik na imagem para saber mais... :)</h1>
+            <a className='fotowallet' href='https://paulynho-lima.github.io/star-wars/' target="_blank"><img src={fotoStar} alt="star-wars" class="item current-item" /></a>
           </SwiperSlide>
 
           <SwiperSlide>
-            <h1>Esta é aplicação Back-End desenvolvido um CRUD utilizando o BD. MongoDB na arquitetura MSC seguindo os principíos de API RESTs e a autenticação foi feita via JWT. Clik na imagem para saber mais... :)</h1>
+            <h1>Esta é aplicação Back-End Nodejs, desenvolvido um CRUD utilizando o BD. MongoDB na arquitetura MSC seguindo os principíos de API RESTs e a autenticação foi feita via JWT. Clik na imagem para saber mais... :)</h1>
             <a className='fotowallet' href='https://github.com/Paulynho-lima/Projeto-Cookmaster' target="_blank"><img src={fotoCook} alt="cookmaster" class="item current-item" /></a>
+          </SwiperSlide>
+
+          <SwiperSlide>
+            <h1>Esta é aplicação Back-End Nodejs, desenvolvido um CRUD de Blog-Posts utilizando o BD. MySql com Sequelize na arquitetura MSC seguindo os principíos de API RESTs e a autenticação foi feita via JWT. Clik na imagem para saber mais... :)</h1>
+            <a className='fotowallet' href='https://github.com/Paulynho-lima/Blogs-Api' target="_blank"><img src={fotoBlog} alt="blog-posts" class="item current-item" /></a>
           </SwiperSlide>
         </Carossel>
 
