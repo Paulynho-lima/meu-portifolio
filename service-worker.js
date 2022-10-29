ca534e5da77d1a5ef9e7687ca8f4e244cffe5f5e
Paulo Lima <paulynho_lima@hotmail.com>
2022-10-28 21:25:13 -0300

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/meu-portifolio/precache-manifest.3dc23f9a5a1c1cc4a76ab16258dd49e3.js"
+  "/meu-portifolio/precache-manifest.3829dd49553aa0424cdf05b54cbde6d1.js"
 );
 
 self.addEventListener('message', (event) => {
